9e5c263b46bb12f28ab3bf6c29716adce12c4513
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-06-04 18:23:52 +0430

    feat: :art: added resolve_decks to schema

--- apps/schema/schema.py
+++ apps/schema/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from apps.users.models import User as UserModel
+from apps.decks.models import Deck as DeckModel
 
 
 class User(DjangoObjectType):
@@ -8,11 +9,20 @@ class User(DjangoObjectType):
         model = UserModel
 
 
+class Deck(DjangoObjectType):
+    class Meta:
+        model = DeckModel
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(User)
+    decks = graphene.List(Deck)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_decks(self, info):
+        return DeckModel.objects.all()
 
 
 schema = graphene.Schema(query=Query)
