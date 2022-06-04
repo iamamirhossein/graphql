f8d3ddf6c6f81e4771120e8683278e10c708aa45
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-06-04 18:07:48 +0430

    refactor: :zap: renamed model

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class TimeStamp(models.Model):
+class Timestamps(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
