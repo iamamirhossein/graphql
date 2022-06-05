3d01af3acddb724a38a23ff29f91ab517ca29fc5
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-06-05 12:36:01 +0430

    feat: :art: added resolve_cards to schema

--- apps/schema/schema.py
+++ apps/schema/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from apps.users.models import User as UserModel
 from apps.decks.models import Deck as DeckModel
+from apps.cards.models import Card as CardModel
 
 
 class User(DjangoObjectType):
@@ -14,15 +15,24 @@ class Deck(DjangoObjectType):
         model = DeckModel
 
 
+class Card(DjangoObjectType):
+    class Meta:
+        model = CardModel
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(User)
     decks = graphene.List(Deck)
+    cards = graphene.List(Card)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
 
     def resolve_decks(self, info):
         return DeckModel.objects.all()
+
+    def resolve_cards(self, info):
+        return CardModel.objects.all()
 
 
 schema = graphene.Schema(query=Query)
